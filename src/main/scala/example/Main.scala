e4020f7a611408e7343474a4513c6e54b83f41c1
Alexandr <alexmich@yandex.ru>
2019-10-19 19:46:49 +0300

    Use EitherT as container

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -3,28 +3,74 @@ package example
 import java.time.LocalDateTime
 import java.util.concurrent.Executors
 
-import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Promise}
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success, Try}
+import scala.concurrent.ExecutionContext
+import cats.data._
+import cats.implicits._
 
 object Main extends App {
-  private val blockingEc =
+  private implicit val blockingEc =
     ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
-  def run(ids: List[Int]): BlackResult = {
-    // Если сервис RED недоступен, то надо подставить значение по умолчанию: RedAdapter.defaultResult(id)
-    val red = ids.map(RedAdapter.run)
+  def run(ids: List[Int]): EitherT[Future, FuckingError, BlackResult] = {
+    for {
+      red <- EitherT.right(ids.map(redRunOrDefault(_)).sequence)
+      green <- greenRunOrError(red)(blockingEc)
+      blue <- EitherT.right(blueRun(green))
+      black <- blackRunOrError(blue)
+    } yield black
+  }
 
-    val green = Await.result(GreenAdapter.run(red)(blockingEc), 10.seconds)
+  val run1 = run(1.to(10).toList)
+  val run2 = run(1.to(10).toList)
+  val run3 = run(1.to(10).toList)
 
+  (for {
+    _ <- EitherT.right[FuckingError](Future{println(LocalDateTime.now())})
+    _ <- run1
+    _ <- run2
+    _ <- run3
+    _ <- EitherT.right[FuckingError](Future{println(LocalDateTime.now())})
+  } yield ()).leftMap(
+    e =>
+      println(e)
+  )
+
+  private def redRunOrDefault(id: Int)(implicit ec: ExecutionContext):Future[RedResult] =
+    Future{
+      Try(RedAdapter.run(id)).getOrElse(RedAdapter.defaultResult(id))
+    }
+
+  private def greenRunOrError(red: List[RedResult])(ec: ExecutionContext): EitherT[Future,FuckingError,GreenResult] =
+    EitherT(
+      GreenAdapter.run(red)(ec).transform {
+        case Success(result) => Success(Right(result))
+        case Failure(e) => Success(Left(GreenFuckingError(e.getMessage)))
+      }(ec)
+    )
+
+  private def blueRun(green: GreenResult): Future[BlueResult] = {
     val promise = Promise[BlueResult]()
     BlueAdapter.run(green, promise.success)
-    val blue = Await.result(promise.future, 10.seconds)
-
-    val blackResource = BlackAdapter.init()
-    val black = blackResource.run(blue)
-    blackResource.destroy()
-    black
+    promise.future
   }
-  println(LocalDateTime.now())
-  println(run(1.to(10).toList))
-  println(LocalDateTime.now())
+
+  private def blackRunOrError(blue: BlueResult)(implicit ec: ExecutionContext): EitherT[Future,FuckingError,BlackResult] = {
+    val blackResource = BlackAdapter.init()
+    EitherT(
+      Future {
+        val blackResultOrError =
+          Try(blackResource.run(blue)).fold[Either[BlackFuckingError, BlackResult]](
+            e => Left(BlackFuckingError(e.getMessage)),
+            Right(_)
+          )
+        blackResource.destroy()
+        blackResultOrError
+      }
+    )
+  }
 }
+
+trait FuckingError
+case class GreenFuckingError(message: String) extends FuckingError
+case class BlackFuckingError(message: String) extends FuckingError
